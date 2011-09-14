b9d8ba071bfbe01122000e80b098aac51ad718ba
Sean Coates <sean@seancoates.com>
2011-09-14 00:42:13 -0400

    fix path

--- templates/_entries.html.php
+++ templates/_entries.html.php
@@ -2,7 +2,7 @@
 <h2>Select an entry for <?php echo escape($params['user']); ?></h2>
 <ul>
 	<?php
-	$path = escape(rtrim($_SERVER['REQUEST_URI'], '/'));
+	$path = escape(rtrim(preg_replace('/\?.*$', '', $_SERVER['REQUEST_URI']), '/'));
 	foreach ($params['entries'] as $entry):
 		$safeentry = escape($entry);
 		$safeentryname = escape(substr($entry, 0, strrpos($entry, '.')));
